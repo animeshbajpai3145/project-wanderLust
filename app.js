95786b42a1158e22a870ad9fa70e790ab2801de1
Your GitHub Name <bajpaianimesh13@gmail.com>
2025-12-22 02:05:13 +0530

    Added the user login and signup functionality

--- app.js
+++ app.js
@@ -5,16 +5,15 @@ const mongoose  = require("mongoose");
 const path = require ("path");
 const methodOverride = require("method-override");
 const ExpressError = require("./utils/ExpressError.js");
-const listings= require("./routes/listing.js");
-const reviews = require("./routes/reviews.js");
+const listingRouter= require("./routes/listing.js");
+const reviewRouter= require("./routes/reviews.js");
+const userRouter = require("./routes/user.js");
 const Mongo_Url = "mongodb://127.0.0.1:27017/wanderlust";
 const session = require('express-session');
 const flash = require('connect-flash');
 const passport  = require("passport");
-const localStrategy = require("passport-local");
+const LocalStrategy = require("passport-local");
 const User = require('./models/user.js');
-const passport =require("passport");
-const local = require('passport-local');
 
 
 app.use(methodOverride("_method"));
@@ -38,6 +37,14 @@ app.use(express.static(path.join(__dirname,"public")));
 }); 
 app.use(session(sessionOptions));
 app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
 app.use((req,res,next)=>{
    res.locals.success = req.flash("success");
    res.locals.error = req.flash("error");
@@ -46,8 +53,11 @@ app.use((req,res,next)=>{
 });
 
 
-app.use("/listings", listings);
-app.use('/listings/:id/reviews',reviews);
+
+
+app.use("/listings", listingRouter);
+app.use('/listings/:id/reviews',reviewRouter);
+app.use('/',userRouter);
 
 
 main().then(()=>{
