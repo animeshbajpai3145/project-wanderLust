ec2f593ea7241ab37f5d6b45cc393efcfb5e6bbb
Your GitHub Name <bajpaianimesh13@gmail.com>
2025-12-30 23:59:13 +0530

    file upload

--- routes/listing.js
+++ routes/listing.js
@@ -5,11 +5,17 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const Listing = require("../models/listing.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 const listingController = require("../controllers/listing.js");
+const multer = require('multer');
+const upload = multer({dest: 'uploads/'});
 
 
 
 
-router.route("/").get(wrapAsync(listingController.index)).post( isLoggedIn, validateListing, wrapAsync(listingController.createNewListing));
+router.route("/").get(wrapAsync(listingController.index))
+// .post( isLoggedIn, validateListing, wrapAsync(listingController.createNewListing));
+.post(upload.single('listing[image][url]'),(req,res)=>{
+ res.send(req.file);
+});
 
 
 
